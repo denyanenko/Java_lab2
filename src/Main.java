6c387e59d3715014c12b2af8cf01a12bfb35a4aa
denyanenko <108097508+denyanenko@users.noreply.github.com>
2023-10-25 12:56:11 +0300

    version 2

--- src/Main.java
+++ src/Main.java
@@ -38,12 +38,15 @@ public class Main {
         outputMatrix(A);
         System.out.println("Матриця B");
         outputMatrix(B);
-        float[][] C = new float[n][m];
-        matrixXOR(A, B, C);
-        System.out.println("Матриця C");
-        outputMatrix(C);
-        System.out.println("Сума найменших елементів по стовпцях - " + sumOfSmallestElementsInColumns(C));
-
+        float[][] C;
+        try {
+            C = matrixXOR(A, B);
+            System.out.println("Матриця C");
+            outputMatrix(C);
+            System.out.println("Сума найменших елементів по стовпцях - " + sumOfSmallestElementsInColumns(C));
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
 
     }
 
@@ -69,16 +72,39 @@ public class Main {
 
     }
 
-    static void matrixXOR(float[][] matrixA, float[][] matrixB, float[][] matrixC) {
+    static float[][] matrixXOR(float[][] matrixA, float[][] matrixB) throws Exception {
+        if (matrixA.length != matrixB.length) {
+            throw new Exception("Різна кількість рядків в вхідних матрицях");
+        }
+        if (matrixA[0].length != matrixB[0].length) {
+            throw new Exception("Різна кількість стовпців в вхідних матрицях");
+        }
+        for (int i = 1; i < matrixA.length; i++) {
+            if (matrixA[0].length != matrixA[i].length) {
+                throw new Exception("Матриця А не прямокутна");
+            }
+            if (matrixB[0].length != matrixB[i].length) {
+                throw new Exception("Матриця B не прямокутна");
+            }
+
+        }
+        float[][] matrixC = new float[matrixA.length][matrixA[0].length];
         for (int i = 0; i < matrixA.length; i++) {
             for (int j = 0; j < matrixA[i].length; j++) {
                 matrixC[i][j] = (int) matrixA[i][j] ^ (int) matrixB[i][j];
             }
         }
+        return matrixC;
     }
 
-    public static float sumOfSmallestElementsInColumns(float[][] matrix) {
+    public static float sumOfSmallestElementsInColumns(float[][] matrix) throws Exception {
         float sum = 0;
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[0].length != matrix[i].length) {
+                throw new Exception("Матриця не прямокутна");
+            }
+
+        }
 
         for (int j = 0; j < matrix[0].length; j++) {
             float min = matrix[0][j];
